3ebcf77ddd896e496b7afb893cfe7392adc5d784
Sodolsky <mateuszsodolskiofficial@gmail.com>
2023-02-07 15:39:42 +0100

    Redacted comment

--- components/ValidateYoutubeUrl.tsx
+++ components/ValidateYoutubeUrl.tsx
@@ -11,7 +11,7 @@ export const validateYouTubeUrl = (url: string): boolean | undefined => {
 type youtubeURL = { id: string; timestamp: null | string };
 export const getLinkId = (url: string): youtubeURL => {
   const match = Matchlink(url);
-  //?start=84 This is the format we need to convert from ?t=84
+  //?start=84 This is the format we need to convert from ?t=84 for timestamps to work
   if (match && match[2].length === 11) {
     return { id: match[2], timestamp: !match[4] ? null : `start=${match[4]}` };
   }
